f9665d73a838f34b56f73169497d0d3bc5750416
m1t9 <dpleshanov@gmail.com>
2020-06-23 20:14:16 +0300

    add 2802

---
+++ task28/task2802/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task28.task2802;
+
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+Пишем свою ThreadFactory
+*/
+public class Solution {
+
+    private static final AtomicInteger groupCount = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+        class EmulatorThreadFactoryTask implements Runnable {
+            @Override
+            public void run() {
+                emulateThreadFactory();
+            }
+        }
+
+        ThreadGroup group = new ThreadGroup("firstGroup");
+        Thread thread = new Thread(group, new EmulatorThreadFactoryTask());
+
+        ThreadGroup group2 = new ThreadGroup("secondGroup");
+        Thread thread2 = new Thread(group2, new EmulatorThreadFactoryTask());
+
+        thread.start();
+        thread2.start();
+    }
+
+    private static void emulateThreadFactory() {
+        AmigoThreadFactory factory = new AmigoThreadFactory();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+        factory.newThread(r).start();
+    }
+
+    public static class AmigoThreadFactory implements ThreadFactory {
+
+        private final AtomicInteger threadCount = new AtomicInteger(0);
+        private final ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        private int fabricNumber;
+
+        public AmigoThreadFactory() {
+            this.fabricNumber = Solution.groupCount.incrementAndGet();
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = threadFactory.newThread(r);
+            thread.setDaemon(false);
+            thread.setPriority(Thread.NORM_PRIORITY);
+            thread.setName(thread.getThreadGroup().getName() + "-pool-" +
+                    fabricNumber + "-thread-" +
+                    threadCount.incrementAndGet());
+
+            return thread;
+        }
+    }
+}
